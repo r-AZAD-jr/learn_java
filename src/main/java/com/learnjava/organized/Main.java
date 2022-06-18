674fa2f4f85499d7052d316d3e7e0fab533114d9
Rashed Azad <rashed.azad@neura-robotics.com>
2022-06-18 18:59:50 +0200

    RAZ : Operator Precedence
    Signed-off-by: Rashed Azad <rashed.azad@neura-robotics.com>

--- src/main/java/com/learnjava/organized/Main.java
+++ src/main/java/com/learnjava/organized/Main.java
@@ -2,21 +2,14 @@ package com.learnjava.organized;
 
 public class Main {
     public static void main(String[] args) {
-        int int32bit = 1;
-        System.out.println("Prefix : " + ++int32bit);
-        int32bit = 1;
-        System.out.println("Postfix : " + int32bit++);
+        int var1 = 10;
+        int var2 = 20;
+        int var3 = 30;
 
-        int32bit = 1;
-        /**
-         * Other Compound Operator
-         * +=
-         * -=
-         * *=
-         * /=
-         * %=
-         * */
-        int32bit += 10;
-        System.out.println("Compound : " + int32bit);
+        int result1 = var1 + var2 / var3;
+        int result2 = (var1 + var2) / var3;
+
+        System.out.println("Result 1 : " + result1);
+        System.out.println("Result 2 : " + result2);
     }
 }
