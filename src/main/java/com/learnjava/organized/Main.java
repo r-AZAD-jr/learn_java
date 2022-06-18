49c29098c8fe88d2a6c1370ba8a1fa6a44633ddc
Rashed Azad <rashed.azad@neura-robotics.com>
2022-06-18 20:39:31 +0200

    RAZ : Added Conditional Operator
    Signed-off-by: Rashed Azad <rashed.azad@neura-robotics.com>

--- src/main/java/com/learnjava/organized/Main.java
+++ src/main/java/com/learnjava/organized/Main.java
@@ -3,21 +3,10 @@ package com.learnjava.organized;
 public class Main {
     public static void main(String[] args) {
 
-        float floatVal = 10.5f;
-        double doubleVal = 10.5d;
-        byte byteVal = 10;
-        short shortVal = 10;
-        int intVal = 10;
-        long longVal = 10L;
+        boolean result1 = 'c' > 'a';
+        boolean result2 = 'd' > 'e';
 
-        // Narrowing Conversion is not safe.
-        // Be careful when do so !!
-        byte byteVal2 = (byte) intVal;
-
-        // Widening Conversion is ok.
-        // Implicit conversion.
-        long longVal2 = intVal;
-
-        System.out.println("Success !!");
+        System.out.println("Is c grater then a : " + result1);
+        System.out.println("Is d grater then e : " + result2);
     }
 }
