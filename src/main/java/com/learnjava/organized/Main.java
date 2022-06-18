29a1d3df0689e7db085473bc4be3216a52361818
Rashed Azad <rashed.azad@neura-robotics.com>
2022-06-19 01:26:13 +0200

    RAZ : Added Loops and Array
    Signed-off-by: Rashed Azad <rashed.azad@neura-robotics.com>

--- src/main/java/com/learnjava/organized/Main.java
+++ src/main/java/com/learnjava/organized/Main.java
@@ -3,20 +3,25 @@ package com.learnjava.organized;
 public class Main {
     public static void main(String[] args) {
 
-        String country = "germany";
-
-        System.out.print("Spoken Language : ");
-        switch (country) {
-            case "germany":
-                System.out.println("German");
-                break;
-            case "usa":
-            case "canada":
-                System.out.println("English");
-                break;
-            default:
-                System.out.println("No Country for Old Men");
-                break;
+        byte counter = 1;
+        while (counter < 10) {
+            System.out.println("While Counter : " + counter);
+            counter *= 2;
         }
+
+        counter = 1;
+        do {
+            System.out.println("Do While Counter : " + counter);
+            counter *= 2;
+        }
+        while (counter <= 1);
+
+        for (int i = 1; i < 10; i *= 2)
+            System.out.println("For loop I : " + i);
+
+        double[] doubleArray = {10.1d,20.2d,30.3d};
+        System.out.print("Array : ");
+        for(Object Item : doubleArray)
+            System.out.print(Item + " ");
     }
 }
