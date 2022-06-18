439ebe61dd7a47d19a805eb3a5a4ab3cfd57f0c4
Rashed Azad <rashed.azad@neura-robotics.com>
2022-06-18 18:19:21 +0200

    RAZ : Added primitive datatype for Java
    Signed-off-by: Rashed Azad <rashed.azad@neura-robotics.com>

---
+++ src/main/java/com/learnjava/organized/Main.java
@@ -0,0 +1,16 @@
+package com.learnjava.organized;
+
+public class Main {
+    public static void main(String[] args) {
+        byte byte8bit = 100;
+        short short16bit = 1000;
+        int int32bit = 1;
+        long long64bit = 1L;
+
+        float float32bit = 0.01f;
+        double double64bit = 0.01;
+        char charOneLetter = 'U';
+        boolean boolTrue = true;
+        boolTrue = false;
+    }
+}
