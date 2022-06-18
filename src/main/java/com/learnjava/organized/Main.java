2dd7cd37e20b760812799ce50f5373599f45d7d1
Rashed Azad <rashed.azad@neura-robotics.com>
2022-06-18 23:26:52 +0200

    RAZ : Added Switch Case
    Signed-off-by: Rashed Azad <rashed.azad@neura-robotics.com>

--- src/main/java/com/learnjava/organized/Main.java
+++ src/main/java/com/learnjava/organized/Main.java
@@ -3,19 +3,20 @@ package com.learnjava.organized;
 public class Main {
     public static void main(String[] args) {
 
-        String country = "Germany";
+        String country = "germany";
 
         System.out.print("Spoken Language : ");
-
-        if (country=="Germany")
-            System.out.println("German");
-        else if (country=="Germany")
-            System.out.println("Deutsch");
-        else if (country=="usa")
-            System.out.println("English");
-        else if (country=="canada")
-            System.out.println("English");
-        else
-            System.out.println("No Country for Old Men");
+        switch (country) {
+            case "germany":
+                System.out.println("German");
+                break;
+            case "usa":
+            case "canada":
+                System.out.println("English");
+                break;
+            default:
+                System.out.println("No Country for Old Men");
+                break;
+        }
     }
 }
