033ae89aec19684b58aec57d7f010816ece4b371
Rashed Azad <rashed.azad@neura-robotics.com>
2022-06-19 16:00:06 +0200

    RAZ : Added String Builder
    Signed-off-by: Rashed Azad <rashed.azad@neura-robotics.com>

--- src/main/java/com/learnjava/organized/Main.java
+++ src/main/java/com/learnjava/organized/Main.java
@@ -1,33 +1,12 @@
 package com.learnjava.organized;
 
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        handleWorkFlow(args);
-    }
-
-    private static void handleWorkFlow(String[] args) {
-        int result = args.length > 0 ? handleArgs(args[0]) : handleUserInput();
-        System.out.println("Number is : " + result);
-    }
-
-    private static int handleUserInput() {
-        Scanner scannerObj = new Scanner(System.in);
-        System.out.print( "Enter a Number : " );
-        return Integer.parseInt(scannerObj.nextLine());
-    }
-
-    private static int handleArgs(String args) {
-        String[] stringArray = {
-                "zero",
-                "one",
-                "two",
-                "three"
-        };
-
-        var listOfStringArray = Arrays.asList(stringArray);
-        return listOfStringArray.contains(args) ? listOfStringArray.indexOf(args) : -1;
+        StringBuilder stringBuilderVar = new StringBuilder();
+        stringBuilderVar.append("Hello");
+        stringBuilderVar.append(" String");
+        stringBuilderVar.append(" Builder");
+        stringBuilderVar.append(" !!");
+        System.out.print(stringBuilderVar.toString());
     }
 }
