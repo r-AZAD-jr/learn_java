1906f0ca88569a91b119141b482b75f2284d5118
Rashed Azad <rashed.azad@neura-robotics.com>
2022-06-21 17:14:22 +0200

    RAZ : Added Static initialization block and static import
    Signed-off-by: Rashed Azad <rashed.azad@neura-robotics.com>

--- src/main/java/com/learnjava/organized/Flight.java
+++ src/main/java/com/learnjava/organized/Flight.java
@@ -4,29 +4,36 @@ public class Flight {
     private int passenger;
     private static int seats;
 
-    public int getSeats()
-    {
+    public int getSeats() {
         return seats;
     }
-    public void setSeats(int seats)
-    {
+
+    public void setSeats(int seats) {
         /**
          * instance member can access instance and static member.
          * */
         Flight.seats = seats;
     }
 
-    public static void resetSeats()
-    {
+    public static void resetSeats() {
         seats = 0;
         /**
          * passenger = 0; will generate error, cause static method can only access static member
          * */
     }
 
-    {
-        passenger = 10;
+    /**
+     * Static Initialization Block is used to initialize
+     * Static member of class.
+     * */
+    static {
+        System.out.println("Initializing block of Static field !!");
         seats = 0;
+    }
+
+    {
+        System.out.println("Initializing block instance feild !!");
+        passenger = 10;
     }
 
     public Flight() {
@@ -45,6 +52,6 @@ public class Flight {
 
     public void printFlightInfo() {
         System.out.println("Number of passenger        : " + seats);
-        System.out.println("Number of seats available  : " + (passenger-seats));
+        System.out.println("Number of seats available  : " + (passenger - seats));
     }
 }
--- src/main/java/com/learnjava/organized/Main.java
+++ src/main/java/com/learnjava/organized/Main.java
@@ -1,5 +1,10 @@
 package com.learnjava.organized;
 
+/**
+ * If we want to import all static members
+ * Flight.* can be used to import all.
+ * */
+import static com.learnjava.organized.Flight.resetSeats;
 public class Main {
     public static void main(String[] args) {
         handleFlight();
@@ -9,6 +14,13 @@ public class Main {
         Flight fxd1 = new Flight();
         fxd1.addPassenger(3);
         fxd1.setSeats(7);
+        fxd1.printFlightInfo();
+
+        /**
+         * Because of static import we don't need class name to qualify this method call
+         */
+        resetSeats();
+
         fxd1.printFlightInfo();
     }
 }
