edd05ccb3c55d4c7b0a4aafb83f8c79d806d2d1f
Rashed Azad <rashed.azad@neura-robotics.com>
2022-06-22 15:32:02 +0200

    RAZ : Added method(function) override
    Signed-off-by: Rashed Azad <rashed.azad@neura-robotics.com>

--- src/main/java/com/learnjava/organized/Main.java
+++ src/main/java/com/learnjava/organized/Main.java
@@ -6,47 +6,31 @@ public class Main {
     }
 
     private static void handleFlight() {
-        Flight fxd1 = new Flight();
-        fxd1.addPassenger(3);
-        fxd1.printFlightInfo();
+        print();
 
-        Flight fxd2 = new Flight();
-        fxd2.addPassenger(4);
-        fxd2.printFlightInfo();
+        int age = 20;
+        print(age);
+        
+        short jrAge = 1;
+        /**
+         * implicit widening done by compiler
+         * */
+        print(jrAge);
 
-        swapReference(fxd1, fxd2);
-        System.out.println("\nAfter Swap Reference !!");
-        fxd1.printFlightInfo();
-        fxd2.printFlightInfo();
-
-        swapSeatsAvailable(fxd1,fxd2);
-        System.out.println("\nAfter Changing object instance member !!");
-        fxd1.printFlightInfo();
-        fxd2.printFlightInfo();
+        int msAge = 20;
+        String msName = "love";
+        print(msAge,msName);
     }
 
-    /**
-     * Swap Reference will not work.
-     * Because fxd1 and fxd2 both are
-     * local variable to this method.
-     * Any changes to this reference
-     * itself is local as well.
-     * */
-    private static void swapReference(Flight fxd1, Flight fxd2) {
-        Flight tempObj = fxd1;
-        fxd1 = fxd2;
-        fxd2 = tempObj;
+    private static void print(int msAge, String msName) {
+        System.out.println("Ms age is : " + msAge + ", Ms name is : " + msName);
     }
 
-    /**
-     * We are changing the content of
-     * the object. If changing object
-     * content this is visible in method
-     * as well as outside the method as well.
-     * */
-    private static void swapSeatsAvailable(Flight fxd1, Flight fxd2) {
-        int fxd1Seats = fxd1.getSeats();
-        fxd1.setSeats(fxd2.getSeats());
-        fxd2.setSeats(fxd1Seats);
+    private static void print(int age) {
+        System.out.println("Age is : " + age);
+    }
+
+    private static void print() {
+        System.out.println("Inside Print !!");
     }
 }
