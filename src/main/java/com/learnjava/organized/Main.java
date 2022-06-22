de194529f8ebd38e2efd4a8eb1d69c1e61e32dfc
Rashed Azad <rashed.azad@neura-robotics.com>
2022-06-22 16:14:12 +0200

    RAZ : Added Variable Number of Parameters
    Signed-off-by: Rashed Azad <rashed.azad@neura-robotics.com>

--- src/main/java/com/learnjava/organized/Main.java
+++ src/main/java/com/learnjava/organized/Main.java
@@ -6,31 +6,18 @@ public class Main {
     }
 
     private static void handleFlight() {
-        print();
-
-        int age = 20;
-        print(age);
-        
-        short jrAge = 1;
-        /**
-         * implicit widening done by compiler
-         * */
-        print(jrAge);
-
-        int msAge = 20;
-        String msName = "love";
-        print(msAge,msName);
+        int classLevel          =   9;
+        String firstStudent     =   "Billy";
+        String secondStudent    =   "Joel";
+        print(classLevel, firstStudent, secondStudent);
     }
 
-    private static void print(int msAge, String msName) {
-        System.out.println("Ms age is : " + msAge + ", Ms name is : " + msName);
-    }
-
-    private static void print(int age) {
-        System.out.println("Age is : " + age);
-    }
-
-    private static void print() {
-        System.out.println("Inside Print !!");
+    /**
+     * Only the last variable is allowed to have variable parameters.
+     * */
+    private static void print(int classLevel, String... studentName) {
+        System.out.println("Class Level : " + classLevel + " !!");
+        for(short i = 0; i < studentName.length; ++i)
+            System.out.format("Student %d, Name %s !!\n", i, studentName[i]);
     }
 }
