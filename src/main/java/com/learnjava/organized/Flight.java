ef73a847b296f625c7eb4d741a53eb3db011c25c
Rashed Azad <rashed.azad@neura-robotics.com>
2022-06-22 22:52:38 +0200

    RAZ : Added Concept of super keyword
    Signed-off-by: Rashed Azad <rashed.azad@neura-robotics.com>

--- src/main/java/com/learnjava/organized/Flight.java
+++ src/main/java/com/learnjava/organized/Flight.java
@@ -4,6 +4,21 @@ public class Flight {
     @Override
     public boolean equals(Object o)
     {
+        /**
+         * super calls method from base class.
+         * Even when we override the method
+         * in derived class, we can use super
+         * to call method from base.
+         * */
+        if (super.equals(o))
+            return true;
+
+        /**
+         * Ideally the following check should
+         * be done with one of class field member.
+         * Here just for understanding instanceof
+         * is used.
+         * */
         return o instanceof Flight;
     }
 }
