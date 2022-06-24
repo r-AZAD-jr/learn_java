9b852bc3bfdb2a768875850310991cc1bdf09af4
rashed-azad <rashed.azad@gmail.com>
2022-06-24 16:37:44 +0200

    RAZ : Added Collection ArrayList
    Signed-off-by: rashed-azad <rashed.azad@gmail.com>

--- src/main/java/com/learnjava/organized/Main.java
+++ src/main/java/com/learnjava/organized/Main.java
@@ -1,25 +1,48 @@
 package com.learnjava.organized;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
 public class Main {
     public static void main(String[] args) {
         handleFlight();
     }
 
     private static void handleFlight() {
+        Collection<Passenger> objArrayPassenger = new ArrayList<>();
+        Passenger objPassenger1 = new Passenger("Mark", 3, 600);
+        Passenger objPassenger2 = new Passenger("Mat", 2, 100);
+        Passenger objPassenger3 = new Passenger("Missy", 1, 10);
+        Passenger objPassenger4 = new Passenger("Richard", 2, 200);
 
-        Passenger.Reward objReward = new Passenger.Reward();
-        objReward.userLevel = 3;
-        System.out.println("User Level : " + objReward.userLevel + " !!");
+        Collection<Passenger> objArrayPassenger2 = new ArrayList<>();
+        objArrayPassenger2.add(objPassenger1);
+        objArrayPassenger2.add(objPassenger2);
+        objArrayPassenger2.add(objPassenger3);
 
-        Passenger objPassenger = new Passenger();
-        System.out.println("Top Add : " + objPassenger.getTopPrice() + " !!");
+        objArrayPassenger.addAll(objArrayPassenger2);
+        objArrayPassenger.add(objPassenger4);
 
-        Flight objFlight = new Flight();
-        objFlight.addNewPassenger("Mark", 3, 600);
-        objFlight.addNewPassenger("Mat", 2, 100);
-        objFlight.addNewPassenger("Missy", 1, 10);
-        objFlight.addNewPassenger("Richard", 2, 200);
-        for (var Item : objFlight.getSortedPassengerList())
-            System.out.format("Name : %s, Level : %d, Member for : %d !!\n", Item.getName(), Item.getUserLevel(), Item.getMemberOfDays());
+        System.out.println("First Array Object : " + objArrayPassenger.size() + " !!");
+
+        /**
+         * Removing Item from List is not possible with foreach
+         * */
+        for(var Item : objArrayPassenger2)
+            System.out.println("Second Array By element : " + Item + " !!");
+
+        /**
+         * This is more conventional way to iterate through connection in java !!
+         * */
+        Iterator<Passenger> objIterable = objArrayPassenger.iterator();
+        while (objIterable.hasNext()){
+            Passenger objPassenger = objIterable.next();
+            if(objPassenger.getUserLevel() > 2)
+            {
+                objIterable.remove();
+            }
+        }
+        System.out.println("First Array Object : " + objArrayPassenger.size() + " !!");
     }
 }
