3b9b0483707baf9d13f0f74400b38f7cddec4684
rashed-azad <rashed.azad@gmail.com>
2022-06-25 02:54:36 +0200

    RAZ : Added Map Example
    Signed-off-by: rashed-azad <rashed.azad@gmail.com>

--- src/main/java/com/learnjava/organized/Main.java
+++ src/main/java/com/learnjava/organized/Main.java
@@ -1,8 +1,6 @@
 package com.learnjava.organized;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,39 +8,16 @@ public class Main {
     }
 
     private static void handleFlight() {
-        Collection<Passenger> objArrayPassenger = new ArrayList<>();
-        Passenger objPassenger1 = new Passenger("Mark", 3, 600);
-        Passenger objPassenger2 = new Passenger("Mat", 2, 100);
-        Passenger objPassenger3 = new Passenger("Missy", 1, 10);
-        Passenger objPassenger4 = new Passenger("Richard", 2, 200);
+        Map<Integer, Passenger> objMap = new HashMap<>();
 
-        Collection<Passenger> objArrayPassenger2 = new ArrayList<>();
-        objArrayPassenger2.add(objPassenger1);
-        objArrayPassenger2.add(objPassenger2);
-        objArrayPassenger2.add(objPassenger3);
+        objMap.put(1, new Passenger("Mark", 3, 600));
+        objMap.put(5, new Passenger("Mat", 2, 100));
+        objMap.put(3, new Passenger("Missy", 1, 10));
+        objMap.put(4,new Passenger("Richard", 2, 200));
 
-        objArrayPassenger.addAll(objArrayPassenger2);
-        objArrayPassenger.add(objPassenger4);
-
-        System.out.println("First Array Object : " + objArrayPassenger.size() + " !!");
-
-        /**
-         * Removing Item from List is not possible with foreach
-         * */
-        for(var Item : objArrayPassenger2)
-            System.out.println("Second Array By element : " + Item + " !!");
-
-        /**
-         * This is more conventional way to iterate through connection in java !!
-         * */
-        Iterator<Passenger> objIterable = objArrayPassenger.iterator();
-        while (objIterable.hasNext()){
-            Passenger objPassenger = objIterable.next();
-            if(objPassenger.getUserLevel() > 2)
-            {
-                objIterable.remove();
-            }
-        }
-        System.out.println("First Array Object : " + objArrayPassenger.size() + " !!");
+    objMap.forEach((id, value) ->
+    {
+        System.out.format("ID : %d, Name : %s !!\n", id, value.getName());
+    });
     }
 }
